b2c7f04639b7137d0fa3bfd58dbf21a3d4448211
kim jong soo <rlwhdtn97@naver.com>
2024-06-17 14:13:56 +0900

    문자와 변수를 이어주는방법

--- src/ch02/sec01/VariableUseExample.java
+++ src/ch02/sec01/VariableUseExample.java
@@ -6,6 +6,8 @@ public class VariableUseExample {
         int minute=5;
         System.out.println(
                 hour+"시간"+minute +"분"
+//                문자와 변수를 이어줄 때는 +로 이어운다
+//                문자열은 ""로 감싸주고, 변수는 변수이름만작성
         );
         int totalMinute=(hour*60)+minute;
         System.out.println("총" + totalMinute + "분");
